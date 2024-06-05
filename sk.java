db682108e761d83ad29785dabd1c3c8d2da342f2
Sandeep <sandeeprcb18@gmail.com>
2024-06-05 21:43:53 +0530

    java program

---
+++ sk.java
@@ -0,0 +1,20 @@
+package aits;
+import java.util.*;
+public class Arthematic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List obj =new ArrayList();
+		obj.add(5);
+		obj.add("Sandeep");
+		obj.add("AITS");
+		System.out.println(obj);
+//		Object a[]=obj.toArray();
+//		for(Object x:a) {
+//			System.out.println(x);
+//		}
+		
+
+	}
+
+}
